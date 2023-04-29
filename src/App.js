760c233f392b2bb84f2e4d70573b52b9a1956b18
DeyDipankar <deydipankar826@gmail.com>
2023-04-30 01:23:14 +0530

    initial template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const categories = [
+    { 'id': 1, 'name': 'Hats' },
+    { 'id': 2, 'name': 'Jackets' }, ,
+    { 'id': 3, 'name': 'Shoes' }, ,
+    { 'id': 4, 'name': 'Womens' }, ,
+    { 'id': 5, 'name': 'Mens' },
+  ]
   return (
-    <div className="App">
-      Hello World
+    <div className='categories-container'>
+      {categories.map(item => {
+        return (
+          <div key={item.id} className="category-container">
+            <div className='category-body-container'>
+              {/* <img></img> */}
+              <h2>{item.name}</h2>
+              <p>Buy now</p>
+            </div>
+          </div>
+        )
+      }
+      )}
     </div>
   );
 }
